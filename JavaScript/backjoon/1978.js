eb741c936b03bcebeb2a3923db46b68247776dff
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-10-11 23:58:40 +0900

    231011-1978.js

--- JavaScript/backjoon/1978.js
+++ JavaScript/backjoon/1978.js
@@ -3,28 +3,26 @@ let input;
 try {
   input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 } catch {
-  input = `4
-1 3 5 7`
+  input = `10
+0 1 2 3 4 5 6 7 8 9`
     .trim()
     .split("\n");
 }
-
-let N = +input[0];
 let count = 0;
-for (let i = 0; i < N; i++) {
-  let num = +input[1].split(" ")[i];
-  let flag = 0;
-  if (num > 1) {
-    for (let j = 2; j <= Math.ceil(Math.sqrt(num)); j++) {
-      if (num % j === 0) {
-        flag = 1;
-        break;
-      }
+let num = input[1].split(" ").map(Number);
+let num3 = num.filter((n) => {
+  return (n % 2 !== 0 && n > 1) || n === 2;
+});
+
+for (let i = 0; i < num3.length; i++) {
+  let flag = 1;
+  for (let j = 2; j <= Math.sqrt(num3[i]); j++) {
+    if (num3[i] % j === 0) {
+      flag = 0;
+      break;
     }
-  } else {
-    flag = 1;
   }
-  if (flag === 0) {
+  if (flag === 1) {
     count++;
   }
 }
